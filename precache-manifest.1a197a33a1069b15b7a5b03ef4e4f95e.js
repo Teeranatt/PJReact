73c2e6943db97c32871aed05c909bf8dcd406a77
Teeranatt <teeranat.sr.61@ubu.ac.th>
2020-09-02 11:58:08 +0700

    Updates

---
+++ precache-manifest.1a197a33a1069b15b7a5b03ef4e4f95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84a84b4a8fb3027cb5926c7b0b0fc00",
+    "url": "/PJReact/index.html"
+  },
+  {
+    "revision": "957816141bfbc6bb6182",
+    "url": "/PJReact/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "6b10f922532e92bb6644",
+    "url": "/PJReact/static/js/2.a5f382e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PJReact/static/js/2.a5f382e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957816141bfbc6bb6182",
+    "url": "/PJReact/static/js/main.d840da9e.chunk.js"
+  },
+  {
+    "revision": "18f356ccf6de79388326",
+    "url": "/PJReact/static/js/runtime-main.9ab24825.js"
+  }
+]);
